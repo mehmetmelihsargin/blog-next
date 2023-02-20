4475646383174b815b8dbd401b1d912d2a603df5
MmSARGIN <melih.sargin.53@gmail.com>
2023-02-20 11:58:19 +0300

    no-auth

--- app/login/page.js
+++ app/login/page.js
@@ -1,66 +1,66 @@
-'use client';
-import React, { useState } from 'react';
-import Navbar from '@/components/Navbar';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebase';
-import { login } from '@/store/auth';
-import { useDispatch } from 'react-redux';
+// 'use client';
+// import React, { useState } from 'react';
+// import Navbar from '@/components/Navbar';
+// import { signInWithEmailAndPassword } from 'firebase/auth';
+// import { auth } from '../../firebase';
+// import { login } from '@/store/auth';
+// import { useDispatch } from 'react-redux';
 
-function Login() {
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('');
-  const dispatch = useDispatch()
-  const handleSubmit = async e => {
-    e.preventDefault();
-    try {
-      const { user } = await signInWithEmailAndPassword(auth, email, password)
-      console.log("Giriş Yapildi")
-      dispatch(login(user));
-      return user;
-    } catch (error) {
-      console.log(error)
-    }
+// function Login() {
+//   const [email, setemail] = useState('');
+//   const [password, setpassword] = useState('');
+//   const dispatch = useDispatch()
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     try {
+//       const { user } = await signInWithEmailAndPassword(auth, email, password)
+//       console.log("Giriş Yapildi")
+//       dispatch(login(user));
+//       return user;
+//     } catch (error) {
+//       console.log(error)
+//     }
 
-  };
-  return (
-    <main>
-      <Navbar />
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col justify-center items-center border-2"
-      >
-        <label htmlFor="email">Email Address</label>
-        <input
-          className="border-2"
-          id="email"
-          name="email"
-          type="email"
-          onChange={e => setemail(e.target.value)}
-          value={email}
-        />
-        <label htmlFor="email">Password</label>
-        <input
-          className="border-2"
-          id="password"
-          name="password"
-          type="password"
-          onChange={e => setpassword(e.target.value)}
-          value={password}
-        />
+//   };
+//   return (
+//     <main>
+//       <Navbar />
+//       <form
+//         onSubmit={handleSubmit}
+//         className="flex flex-col justify-center items-center border-2"
+//       >
+//         <label htmlFor="email">Email Address</label>
+//         <input
+//           className="border-2"
+//           id="email"
+//           name="email"
+//           type="email"
+//           onChange={e => setemail(e.target.value)}
+//           value={email}
+//         />
+//         <label htmlFor="email">Password</label>
+//         <input
+//           className="border-2"
+//           id="password"
+//           name="password"
+//           type="password"
+//           onChange={e => setpassword(e.target.value)}
+//           value={password}
+//         />
 
-        <button
-          type="submit"
-          className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-        >
-          <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
+//         <button
+//           type="submit"
+//           className="group relative inline-block text-sm font-medium text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+//         >
+//           <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-          <span className="relative block border border-current bg-white px-8 py-3">
-            Submit
-          </span>
-        </button>
-      </form>
-    </main>
-  );
-}
+//           <span className="relative block border border-current bg-white px-8 py-3">
+//             Submit
+//           </span>
+//         </button>
+//       </form>
+//     </main>
+//   );
+// }
 
-export default Login;
+// export default Login;
--- store/store.js
+++ store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import auth from './auth'
+// import auth from './auth'
 import posts from './postData'
 
 export const store = configureStore({
 
     reducer: {
-        auth,
+        // auth,
         posts
     },
 })
